b516c746bd4456abcf452abf0f5f68ca5bb0f4aa
Hossam Mabed <hossam.nasr99@gmail.com>
2020-12-06 03:22:38 -0500

    added new activities and navigations

--- app/src/main/java/edu/harvard/mobihabibi/MainActivity.kt
+++ app/src/main/java/edu/harvard/mobihabibi/MainActivity.kt
@@ -1,11 +1,30 @@
 package edu.harvard.mobihabibi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnEnc = findViewById<Button>(R.id.btnEnc)
+        val btnDec = findViewById<Button>(R.id.btnDec)
+        btnEnc.setOnClickListener {
+            navigateToEnc()
+        }
+        btnDec.setOnClickListener {
+            navigateToDec()
+        }
+    }
+
+    private fun navigateToEnc() {
+        startActivity(Intent(this, EncryptActivity::class.java))
+    }
+
+    private fun navigateToDec() {
+        startActivity(Intent(this, DecryptActivity::class.java))
     }
 }
