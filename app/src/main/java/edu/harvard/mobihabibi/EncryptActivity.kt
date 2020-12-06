1d8e89de43f4d1c0e6851ac9b9884379ad8484dd
Hossam Mabed <hossam.nasr99@gmail.com>
2020-12-06 03:40:19 -0500

    barebones upload activity

--- app/src/main/java/edu/harvard/mobihabibi/EncryptActivity.kt
+++ app/src/main/java/edu/harvard/mobihabibi/EncryptActivity.kt
@@ -2,10 +2,36 @@ package edu.harvard.mobihabibi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class EncryptActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_encrypt)
+
+        val btnUploadSecret = findViewById<Button>(R.id.btnUploadSecret)
+        val btnUploadDecoy = findViewById<Button>(R.id.btnUploadSecret)
+        val btnEncrypt = findViewById<Button>(R.id.btnEncRes)
+        btnUploadSecret.setOnClickListener {
+            uploadSecret()
+        }
+        btnUploadDecoy.setOnClickListener {
+            uploadDecoy()
+        }
+        btnEncrypt.setOnClickListener {
+            encrypt()
+        }
+    }
+
+    private fun uploadSecret() {
+
+    }
+
+    private fun uploadDecoy() {
+
+    }
+
+    private fun encrypt() {
+
     }
 }
