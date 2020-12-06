19d495ad8c0e3f7216d5187a8db050e147deda78
Hossam Mabed <hossam.nasr99@gmail.com>
2020-12-06 04:08:25 -0500

    add uploading and processing images

--- app/src/main/java/edu/harvard/mobihabibi/EncryptActivity.kt
+++ app/src/main/java/edu/harvard/mobihabibi/EncryptActivity.kt
@@ -1,34 +1,84 @@
 package edu.harvard.mobihabibi
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 
 class EncryptActivity : AppCompatActivity() {
+    private var secretBitmap: Bitmap? = null
+    private var decoyBitmap: Bitmap? = null
+
+    companion object {
+        private const val SECRET_PICK_CODE = 999
+        private const val DECOY_PICK_CODE = 1000
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_encrypt)
 
         val btnUploadSecret = findViewById<Button>(R.id.btnUploadSecret)
-        val btnUploadDecoy = findViewById<Button>(R.id.btnUploadSecret)
+        val btnUploadDecoy = findViewById<Button>(R.id.btnUploadDecoy)
         val btnEncrypt = findViewById<Button>(R.id.btnEncRes)
         btnUploadSecret.setOnClickListener {
-            uploadSecret()
+            requestSecret()
         }
         btnUploadDecoy.setOnClickListener {
-            uploadDecoy()
+            requestDecoy()
         }
         btnEncrypt.setOnClickListener {
             encrypt()
         }
     }
 
-    private fun uploadSecret() {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK) {
+            val uri = data?.data
+            if (uri != null) {
+                when (requestCode) {
+                    SECRET_PICK_CODE -> {
+                        processSecret(uri)
+                    }
+                    DECOY_PICK_CODE -> {
+                        processDecoy(uri)
+                    }
+                }
+            }
 
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
-    private fun uploadDecoy() {
+    private fun requestSecret() {
+        requestImage(SECRET_PICK_CODE)
+    }
 
+    private fun processSecret(uri: Uri) {
+        findViewById<ImageView>(R.id.ivSecret).setImageURI(uri)
+        secretBitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
+    }
+
+    private fun requestDecoy() {
+        requestImage(DECOY_PICK_CODE)
+    }
+
+    private fun processDecoy(uri: Uri) {
+        findViewById<ImageView>(R.id.ivDecoy).setImageURI(uri)
+        decoyBitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
+    }
+
+    private fun requestImage(code: Int) {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, code)
     }
 
     private fun encrypt() {
